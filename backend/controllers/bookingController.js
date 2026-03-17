2eba7db93adcec30a9d8feef265893a263d97baa
Soham Kulkarni <sohamkulk11@gmail.com>
2026-03-17 22:00:20 +0530

    fix mail on deployment

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -6,7 +6,9 @@ const nodemailer = require("nodemailer");
 const sendConfirmationEmail = async (userEmail, userName, event, quantity, totalPrice) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
