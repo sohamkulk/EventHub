479f4b1d673f72d061535ec19ed345a3b0e7c97f
Soham Kulkarni <sohamkulk11@gmail.com>
2026-03-17 22:11:18 +0530

    fixed mail on deployment

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -7,7 +7,7 @@ const sendConfirmationEmail = async (userEmail, userName, event, quantity, total
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
+      port: 465,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
